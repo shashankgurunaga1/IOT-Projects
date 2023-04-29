f79638c4bd2daa33f6527ef7fb1ad90811783228
Shashank Gurunaga <shashankgurunaga@gmail.com>
2023-04-29 17:05:05 +0530

    Add files via upload

---
+++ blinkingledbysg/blinkingledbysg.ino
@@ -0,0 +1,13 @@
+int ledPin=8; 
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(ledPin, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
